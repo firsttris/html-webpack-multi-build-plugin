7406af9ac7b89b6e7ff6ffd21ad651c0db98b47f
Mathijs Segers <mathijssegers@gmail.com>
2019-02-04 10:53:49 +0100

    Clear old entry files
    This is a potential solution to #3, obviously can use improvements, like support for [hash:10] (any custom hash length number), options which differ from [hash] like chunkhash etc.
    
    I'm not entirely convinced this would be the way to go, let me know what you think? If you believe this could potentially fix things we can expand it, for me this is working rather nicely (I was debugging this.js to see if it clears old files).

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ function HtmlWebpackMultiBuildPlugin(options) {
 
 HtmlWebpackMultiBuildPlugin.prototype = {
     apply: function(compiler) {
+        this.createOutputRegexes(compiler.options);
+
         if (compiler.hooks) {
             // webpack 4 support
             compiler.hooks.compilation.tap('HtmlWebpackMultiBuildPlugin', compilation => {
@@ -27,6 +29,33 @@ HtmlWebpackMultiBuildPlugin.prototype = {
         data.plugin.options.modernScripts = this.js.filter((value) => value.indexOf('legacy') === -1);
         data.plugin.options.legacyScripts = this.js.filter((value) => value.indexOf('legacy') > 0);
         cb(null, data);
+    },
+    createOutputRegexes: function(options) {
+        if (options.output && options.output.filename) {
+            // default webpack entry
+            let entry = ['main'];
+            if (options.entry) {
+                // when object is provided we have custom entry names
+                if (typeof options.entry === 'object') {
+                    entry = Object.keys(options.entry);
+                }
+            }
+            entry.forEach(e => {
+                const outFilePathForEntry = options.output.filename.replace('[name]', e);
+                this.outputFileNameRegex.push(new RegExp(outFilePathForEntry.replace(['[hash]'], '[\\w\\d]{20}')));
+            });
+        }
+    },
+    clearOldScripts: function(data) {
+        this.clearOldScripts(data);
+        this.outputFileNameRegex.forEach(r => {
+            data.assets.js.forEach(a => {
+                // we have one of our entries
+                if (r.test(a)) {
+                  this.js = this.js.filter(j => !r.test(j));
+                }
+            });
+        });
     },
 };
 
